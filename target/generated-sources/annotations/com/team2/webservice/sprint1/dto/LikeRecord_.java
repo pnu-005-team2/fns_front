2000f56336ade7e7494abffb3b807a35dee89e75
HWP <sunghakzz@naver.com>
2019-03-31 16:07:06 +0900

    Init Project
    Init Project.

---
+++ target/generated-sources/annotations/com/team2/webservice/sprint1/dto/LikeRecord_.java
@@ -0,0 +1,20 @@
+package com.team2.webservice.sprint1.dto;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(LikeRecord.class)
+public abstract class LikeRecord_ {
+
+	public static volatile SingularAttribute<LikeRecord, Long> uid;
+	public static volatile SingularAttribute<LikeRecord, Long> lid;
+	public static volatile SingularAttribute<LikeRecord, Long> pid;
+
+	public static final String UID = "uid";
+	public static final String LID = "lid";
+	public static final String PID = "pid";
+
+}
+
---
+++ target/generated-sources/annotations/com/team2/webservice/sprint1/dto/Like_.java
@@ -0,0 +1,20 @@
+package com.team2.webservice.sprint1.dto;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Like.class)
+public abstract class Like_ {
+
+	public static volatile SingularAttribute<Like, Long> uid;
+	public static volatile SingularAttribute<Like, Long> lid;
+	public static volatile SingularAttribute<Like, Long> pid;
+
+	public static final String UID = "uid";
+	public static final String LID = "lid";
+	public static final String PID = "pid";
+
+}
+
