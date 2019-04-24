3d2af82d6a0fb59fadede1a96102a79c4cc6ffbc
yuntake94 <vndejddlxn1@Naver.com>
2019-04-24 17:55:05 +0900

    Timeline_mod
    #BAK9-62

--- src/main/java/com/team2/webservice/sprint1/jpa/testBoardMapper.java
+++ src/main/java/com/team2/webservice/sprint1/jpa/testBoardMapper.java
@@ -1,4 +1,4 @@
-package com.team2.webservice.sprint1.jpa;
+/*package com.team2.webservice.sprint1.jpa;
 
 import com.team2.webservice.sprint1.vo.testBoardVo;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,3 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface testBoardMapper extends JpaRepository<testBoardVo, Integer> {
 
 } // 인터페이스를 생성후, JpaRepository<Entity 클래스, PK타입>을 상속하면 기본적인 CRUD 메소드가 자동생성됨
+
+
+} // 인터페이스를 생성후, JpaRepository<Entity 클래스, PK타입>을 상속하면 기본적인 CRUD 메소드가 자동생성됨
+*/
